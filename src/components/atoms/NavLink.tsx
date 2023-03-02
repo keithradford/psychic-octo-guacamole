972a24a8f3079791e28e501d245543ab0c26fc67
Alex <zthh626@gmail.com>
2023-03-02 15:53:51 -0800

    feat: underline navLink

--- src/components/atoms/NavLink.tsx
+++ src/components/atoms/NavLink.tsx
@@ -1,4 +1,6 @@
+import classNames from 'classnames';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 interface NavLinkProps {
@@ -7,10 +9,15 @@ interface NavLinkProps {
 }
 
 export const NavLink = ({ text, link }: NavLinkProps) => {
+  const { pathname } = useRouter();
+
   return (
     <Link
       href={link}
-      className="px-4 py-2 font-semibold text-primary hover:underline"
+      className={classNames(
+        pathname === link && 'underline',
+        'px-4 py-2 font-semibold text-primary hover:underline',
+      )}
     >
       {text}
     </Link>
