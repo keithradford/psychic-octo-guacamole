503c303e3b55723092d99a73317a3ce63d449a19
Alex <zthh626@gmail.com>
2023-03-16 17:02:27 -0700

    feat: budget page

--- src/pages/budget.tsx
+++ src/pages/budget.tsx
@@ -1,3 +1,5 @@
+import { trpc } from "@/utils/trpc";
+import { useMemo } from "react";
 import { PercentBar } from "../components/atoms";
 import { NextPageWithLayout } from "./_app";
 
@@ -21,19 +23,57 @@ export const BudgetSectionTitle = ({
 };
 
 const BudgetPage: NextPageWithLayout = () => {
+  const financialStatement = trpc.getBudgetByTerm.useQuery({
+    term: "SPRING 2022",
+  });
+
+  const incomeBars = useMemo(() => {
+    if (!financialStatement.data?.budget?.budgetBars) return [];
+
+    return financialStatement.data.budget.budgetBars.filter((bar) => {
+      return bar.isIncome;
+    });
+  }, [financialStatement.data?.budget?.budgetBars]);
+
+  const spendBars = useMemo(() => {
+    if (!financialStatement.data?.budget?.budgetBars) return [];
+
+    return financialStatement.data.budget.budgetBars.filter((bar) => {
+      return !bar.isIncome;
+    });
+  }, [financialStatement.data?.budget?.budgetBars]);
+
   return (
     <div className="flex flex-col w-full space-y-5">
       <div className="flex flex-row flex-grow">
         <BudgetSectionTitle title="Income" max={1000} current={671} />
-        <PercentBar title="Paycheque" max={1000} current={671} />
+        <div className="flex flex-col w-full space-y-5">
+          {incomeBars.map((bar) => {
+            return (
+              <PercentBar
+                key={bar.id}
+                title={bar.title}
+                max={bar.max}
+                current={bar.currentVal}
+              />
+            );
+          })}
+        </div>
       </div>
       <div className="w-full border" />
       <div className="flex flex-row flex-grow">
         <BudgetSectionTitle title="Spending" max={300} current={95} />
         <div className="flex flex-col w-full space-y-5">
-          <PercentBar title="Entertainment" max={100} current={15} />
-          <PercentBar title="Food" max={100} current={30} />
-          <PercentBar title="Transport" max={100} current={50} />
+          {spendBars.map((bar) => {
+            return (
+              <PercentBar
+                key={bar.id}
+                title={bar.title}
+                max={bar.max}
+                current={bar.currentVal}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
