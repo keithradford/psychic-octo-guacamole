857af8e42f5251e212ca262a97b87208e97947f6
Alex <zthh626@gmail.com>
2023-04-06 15:44:24 -0700

    feat: update landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,54 +9,70 @@ export default function Home() {
   if (!session) return <div></div>;
   return (
     <div className="flex flex-col items-center justify-center w-full h-[80vh]">
-      <div className="grid grid-cols-2 grid-rows-2 w-full h-full px-10 py-10">
+      <div className="grid w-full h-full grid-cols-2 grid-rows-2 px-10 py-10">
         {/* yellow square */}
-        <div className="bg-yellow-300 mr-5 mb-5 rounded-2xl">
+        <div
+          style={{
+            backgroundImage:
+              'url("https://www.uvic.ca/_assets/images/landing-heros/naming-ceremony-1800x1012.jpg")',
+            backgroundSize: "cover",
+          }}
+          className="mb-5 mr-5 bg-yellow-300 rounded-2xl"
+        >
           <div className="flex flex-col items-center justify-center w-full h-full">
-            <h1 className="text-2xl font-bold">
+            <h1 className="p-2 text-2xl font-bold text-gray-800 bg-white bg-opacity-90 rounded-xl">
               Welcome back, {session.username}!
             </h1>
           </div>
         </div>
         {/* blue square */}
-        <div className="bg-blue-300 ml-5 mb-5 rounded-2xl">
+        <Link
+          style={{
+            backgroundImage:
+              'url("https://www.uvic.ca/_assets/images/landing-heros/co-op-jumbo-karl-hare1-1800x1012.jpg")',
+            backgroundSize: "cover",
+          }}
+          className="mb-5 ml-5 bg-blue-300 rounded-2xl hover:cursor-pointer"
+          href="/financial"
+        >
           <div className="flex flex-col items-center justify-center w-full h-full">
-            <h1
-              className="text-2xl font-light
-              underline
-              hover:text-green-800
-            "
-            >
-              <Link href="/financial">View your financial statements</Link>
+            <h1 className="p-2 text-2xl font-bold text-gray-800 bg-white bg-opacity-90 rounded-xl">
+              View your financial statements
             </h1>
           </div>
-        </div>
+        </Link>
         {/* green square */}
-        <div className="bg-green-300 mr-5 rounded-2xl">
+        <Link
+          style={{
+            backgroundImage:
+              'url("https://www.uvic.ca/_assets/images/landing-heros/pooja-parmar-jumbotron-1800x1012.jpg")',
+            backgroundSize: "cover",
+          }}
+          className="mr-5 bg-green-300 rounded-2xl hover:cursor-pointer"
+          href="/budget"
+        >
           <div className="flex flex-col items-center justify-center w-full h-full">
-            <h1
-              className="text-2xl font-light
-              underline
-              hover:text-blue-800
-            "
-            >
-              <Link href="/budget">View your budget</Link>
+            <h1 className="p-2 text-2xl font-bold text-gray-800 bg-white bg-opacity-90 rounded-xl">
+              View your budget
             </h1>
           </div>
-        </div>
+        </Link>
         {/* red square */}
-        <div className="bg-red-300 ml-5 rounded-2xl">
+        <Link
+          style={{
+            backgroundImage:
+              'url("https://www.uvic.ca/_assets/images/landing-heros/vikes-presidents-cup-jumbo-1800x1012.jpg")',
+            backgroundSize: "cover",
+          }}
+          className="ml-5 bg-red-300 rounded-2xl hover:cursor-pointer"
+          href="/reporting"
+        >
           <div className="flex flex-col items-center justify-center w-full h-full">
-            <h1
-              className="text-2xl font-light
-              underline
-              hover:text-blue-800
-            "
-            >
-              <Link href="/reporting">View reporting and analysis</Link>
+            <h1 className="p-2 text-2xl font-bold text-gray-800 bg-white bg-opacity-90 rounded-xl">
+              View reporting and analysis
             </h1>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
